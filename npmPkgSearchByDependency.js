9970360a95ab04ec62819ff42fcc45a5c33e038c
AhyoungRyu <fbdkdud93@hanmail.net>
2017-01-17 22:46:57 +0900

    Initial commits

---
+++ npmPkgSearchByDependency.js
@@ -0,0 +1,70 @@
+var RegClient = require('silent-npm-registry-client')
+var stream = require('stream')
+var url = require('url')
+var strfmt = require('util').format
+var prttty = require('prttty').render
+
+// based off
+// https://stackoverflow.com/questions/13657140/how-to-get-all-npm-packages-that-match-a-particular-dependency-in-json-format
+
+function searchUri (registryURL, dependency) {
+  var u = url.parse(registryURL)
+  return url.format({
+    protocol: u.protocol,
+    host: u.host,
+    pathname: /^\/?$/.test(u.pathname) ? '/-/_view/dependedUpon' : u.pathname,
+    query: {
+      startkey: strfmt('["%s"]', dependency),
+      endkey: strfmt('["%s",{}]', dependency),
+      group_level: 3
+    }
+  })
+}
+
+var params = { timeout: 1000 }
+
+// pass dependency
+module.exports = function npmPkgSearchByDependency (dependency, callback) {
+  var options = (typeof dependency === 'object') ? dependency : {
+    dependency: dependency,
+    registryURL: 'https://registry.npmjs.org/',
+    debug: true
+  }
+
+  // to log or not to log
+  var log = options.debug ? console.log.bind(console) : function () {}
+
+  // construct registry client
+  var client = new RegClient({logstream: new stream.Writable()})
+
+  // construct registry url
+  var uri = searchUri(options.registryURL, options.dependency)
+
+  log('Querying', uri)
+
+  client.get(uri, params, function (error, data, raw, res) {
+    // pass errors to callback
+    if (error) {
+      return callback(error)
+    }
+    // data is the parsed data object
+    // raw is the json string
+    // res is the response from couch
+    log('Response', prttty(data || raw))
+
+    callback(null, data.rows.map(function (r) {
+      // This is what a row looks like:
+      //
+      // { value: 1
+      // , key: [ 'gulp-plugin'
+      //        , 'vinyl-fs-mock'
+      //        , 'A fake file system implementation...etc'
+      //        ]
+      // }
+      return {
+        name: r.key[1],
+        description: r.key[2]
+      }
+    }))
+  })
+}
