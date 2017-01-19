fccfe895bad4982bf4418d06d3fb9216f3a66489
AhyoungRyu <fbdkdud93@hanmail.net>
2017-01-20 03:16:53 +0900

    Enable converted pkg.json file to S3

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+var RegClient = require('silent-npm-registry-client')
+var stream = require('stream')
 var npmPkgSearchByDependency = require('./npmPkgSearchByDependency')
-var getFullPkgInfo = require('./getFullPkgInfo')
 
 var dependency = 'zeppelin-vis'
 
-exports.handler = (event, context, callback) => {
+//define S3 modules
+var AWS = require('aws-sdk')
+var s3 = new AWS.S3(
+  {
+    signatureVersion: 'v4' // need to convert v2 -> v4 to avoid conflict
+  }
+);
 
-  console.log('Received event:', JSON.stringify(event, null, 2));
+/*
+  This file is main for the AWS Lambda
+  Input: package's name, description, artifact(_id), license
+  Where: helium-packages S3 bucket
+ */
+exports.handler = (event, context, callback) => {
+  var bucketName = 'helium-packages'
 
   npmPkgSearchByDependency(dependency, function (error, packages) {
     if (error) {
@@ -15,15 +28,49 @@ exports.handler = (event, context, callback) => {
 
     var N = packages.length
     var callbackMsg = []
+    var params = { timeout: 1000 }
+    var client = new RegClient({logstream: new stream.Writable()})
 
     console.log('\nPackages matching \"' + dependency + '\": (' + N + ')\n')
 
     packages.forEach(function (pkg) {
+      // get each package's url
       var registryURL = 'https://registry.npmjs.org/' + pkg.name + '/latest'
       callbackMsg.push(registryURL)
-    })
 
-    var jsonStringMsg = JSON.parse(JSON.stringify(callbackMsg).replace(/\'/g, ''))
-    callback(null, jsonStringMsg)
+      // get each package's name, desc, artifact and license info
+      client.get(registryURL, params, function (error, data) {
+        if (error) {
+          return callback(error)
+        }
+        var result = {
+          name: data.name,
+          description: data.description,
+          artifact: data._id,
+          license: data.license
+        }
+
+        var parsedBody = JSON.stringify(result)
+
+        // upload each file to S3 bucket
+        var param = {
+          Bucket: bucketName,
+          Key: data.name + '.json',
+          Body: parsedBody
+        }
+
+        console.log(param)
+
+        s3.putObject(param, function (err, data) {
+          if (err) console.log(err, err.stack);
+          else console.log(data);
+
+          context.callbackWaitsForEmptyEventLoop = false;
+          callback(null, 'Success message')
+
+        })
+      })
+    })
+    console.log('')
   })
 };
